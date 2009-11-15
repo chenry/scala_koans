111039dbbc77f806a13d45ac8c381964aef98ef9
Carlus Henry <carlushenry@gmail.com>
2009-11-14 23:16:42 -0500

    Adding more tests

--- src/test/scala/AppTest.scala
+++ src/test/scala/AppTest.scala
@@ -12,6 +12,16 @@ class AppTest {
     @Test
     def testAssertFalse() = assertFalse(true)
 
+    @Test
+    def testAssertFalseWithMessage() = assertFalse("should be false", true)
+
+    @Test
+    def testAssertEquals() = {
+	
+	val num1 = 5
+	val num2 = num1 + 1
+	assertEquals(num1,num2)
+    }
 }
 
 
