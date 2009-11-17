e0c91d51c60dbb56985d2becfe25946d34cc5c5d
Carlus Henry <carlushenry@gmail.com>
2009-11-16 22:00:49 -0500

    Adding a new test that will test the creation of Arrays

---
+++ src/test/scala/AboutArraysTest.scala
@@ -0,0 +1,35 @@
+import org.junit._
+import Assert._
+
+@Test
+class AboutArraysTest{
+
+    @Test
+    def testCreateArraysInstanceOf() = {
+	var a : Array[String] = new Array(0)
+	assertEquals("Should be instance of Array", a.isInstanceOf[String])
+    }
+
+    @Test
+    def testCreateArraysEmptySize() = {
+	var a : Array[String] = new Array(0)
+	assertEquals("Should be size of 0", 1, a.size)
+    }
+
+    @Test
+    def testCreateArraysEmptyLength() = {
+	var a : Array[String] = new Array(0)
+	assertEquals("Should be size of 0", 1, a.length)
+    }
+
+    @Test
+    def testAddValueToArray() = {
+	var a : Array[String] = new Array(2)
+	a(0) = "1"
+	assertEquals("Should be 1", "2", a(0))
+    }
+
+
+}
+
+
